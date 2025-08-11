f60cb22f7af1c93744a216e7ca2ec868069da48e
Christian Hinkle <christianhinko@gmail.com>
2025-08-10 18:20:48 -0600

    [Gem] Recreate gem using the engine's ProjectManager this time.
    - Use the "default" template.
    - Enable all platforms.
    - Pass in the name of the gem for any required properties (e.g., the license).
    - We will fill out the gem.json more properly later.

--- Code/Include/O3DEGemPlaygroundCH/O3DEGemPlaygroundCHTypeIds.h
+++ Code/Include/O3DEGemPlaygroundCH/O3DEGemPlaygroundCHTypeIds.h
@@ -4,16 +4,16 @@
 namespace O3DEGemPlaygroundCH
 {
     // System Component TypeIds
-    inline constexpr const char* O3DEGemPlaygroundCHSystemComponentTypeId = "{D8F0EC9E-6EF0-44FD-AAD8-1C54D897AD5A}";
-    inline constexpr const char* O3DEGemPlaygroundCHEditorSystemComponentTypeId = "{354DC0DD-DF99-4EFE-8EEE-E5FFA32FFA29}";
+    inline constexpr const char* O3DEGemPlaygroundCHSystemComponentTypeId = "{CFC6FEBF-7D15-4B04-B163-77C534D125DF}";
+    inline constexpr const char* O3DEGemPlaygroundCHEditorSystemComponentTypeId = "{6D80B98C-862B-4F45-8DF8-33F4431683D2}";
 
     // Module derived classes TypeIds
-    inline constexpr const char* O3DEGemPlaygroundCHModuleInterfaceTypeId = "{6CEEB3F8-0D74-4B39-9253-F6FB365648D2}";
-    inline constexpr const char* O3DEGemPlaygroundCHModuleTypeId = "{2053116B-39C5-4E37-BD3F-899423811C03}";
+    inline constexpr const char* O3DEGemPlaygroundCHModuleInterfaceTypeId = "{2F503DBD-D294-4278-BD5F-5CA3C1ACCEE1}";
+    inline constexpr const char* O3DEGemPlaygroundCHModuleTypeId = "{3E165F61-0059-4D79-8508-82003DA5E0BA}";
     // The Editor Module by default is mutually exclusive with the Client Module
     // so they use the Same TypeId
     inline constexpr const char* O3DEGemPlaygroundCHEditorModuleTypeId = O3DEGemPlaygroundCHModuleTypeId;
 
     // Interface TypeIds
-    inline constexpr const char* O3DEGemPlaygroundCHRequestsTypeId = "{0EFCC396-7F3D-4C33-8CA3-E1BFF8C2DE56}";
+    inline constexpr const char* O3DEGemPlaygroundCHRequestsTypeId = "{E1E30DB9-A96B-4288-AAAC-B57F6804A47E}";
 } // namespace O3DEGemPlaygroundCH
